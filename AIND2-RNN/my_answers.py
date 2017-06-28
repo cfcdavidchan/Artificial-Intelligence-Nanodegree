5f5808b3a64fd1f486ba8988000049a149052347
David Chan <cfcdavidchan@gmail.com>
2017-06-28 17:28:54 +0800

    RNN completed

--- AIND2-RNN/my_answers.py
+++ AIND2-RNN/my_answers.py
@@ -12,7 +12,11 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    
+    for i in range(len(series) - window_size):
+        X.append(series[i : i+window_size])
+        y.append(series[i+window_size])
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,14 +27,31 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    np.random.seed(0)
+    model = Sequential()
+    model.add(LSTM(5, input_shape= X_train.shape[1:]))
+    model.add(Dense(len(X_train[0][0])))
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
-
-
+    unique_characters = []
+    list_text = text.split()
+    text_dict = dict(Counter(list_text))
+    for k,v in text_dict.items():
+        if v == 1:
+            unique_characters.append(k)
+    symbols = '${}()[].,:;@"!%?+-*/&|<>=~1234567890'
+    for s in symbols:
+        print('Now removing %s'%s, end='\r')
+        sys.stdout.flush()
+        text = text.replace(s,' ')
+        time.sleep(0.1)
+    text = text.replace('  ',' ')
     # remove as many non-english characters and character sequences as you can 
 
 
@@ -39,7 +60,11 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
-
-    
+    i = 0
+    while i + window_size < len(text):
+        string = text[i:i + window_size]
+        inputs.append(string)
+        outputs.append(text[i + window_size])
+        i += step_size
+        
     return inputs,outputs
